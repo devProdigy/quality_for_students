c8b95360a43a3bcc8bdabc23cceebf0f3f37647e
devProdigy <cherednyk.o@gmail.com>
2021-03-10 10:52:43 +0100

    adjust test and turn off CI checks

--- tests/unit/test_user_bad_code.py
+++ tests/unit/test_user_bad_code.py
@@ -2,5 +2,6 @@ from app.user_bad_code import User
 
 
 def test_user_get_full_name():
-    user = User("Alex", "111")
+    user = User("Alex", "Alexxx", 300)
     assert user.get_full_name() == "Alex 111"
+    assert user.age == 300
